7efaf22605ef5fd62ec3dfdc29641d0738f58add
kopardev <vishal.koparde@nih.gov>
2023-09-30 15:00:04 -0400

    feat: adding some code

---
+++ R/filter_low_counts.R
@@ -0,0 +1,19 @@
+
+#' filter_low_counts
+#'
+#' @param raw_counts_matrix raw_counts_matrix object
+#' @param min_counts integer number of min_counts across all samples, default 0
+#' @param min_cpm float minimum cpm value, default 0
+#' @param min_cpm_fraction float fraction of samples that need to satisfy min_cpm filter, default 1.0
+#'
+#' @return filtered_raw_count_matrix
+#' @export
+#'
+#' @examples
+filter_low_counts <- function(
+    raw_counts_matrix,
+    min_counts = 0,
+    min_cpm = 0,
+    min_cpm_fraction = 1.0){
+
+}
---
+++ R/read_raw_counts.R
@@ -0,0 +1,14 @@
+#' read_raw_counts
+#'
+#' @param pathtofile string absolute file path to raw counts matrix
+#' @param delimiter string default is tab
+#'
+#' @return raw_counts_matrix
+#' @export
+#'
+#' @examples
+read_raw_counts <- function(
+    pathtofile,
+    delimiter = "\t") {
+
+}
