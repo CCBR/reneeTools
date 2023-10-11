945b1b267f1cc4982bada5d8beed1bb4a63a911d
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-10-11 15:28:51 -0400

    feat: create example data from RSEM output

---
+++ data-raw/wt_s1_genes.R
@@ -0,0 +1,5 @@
+## code to prepare `wt_s1_genes` dataset goes here
+
+# WT_S1.RSEM.genes.results was generated from running RENEE v2.5.3 on the test dataset https://github.com/CCBR/RENEE/tree/e08f7db6c6e638cfd330caa182f64665d2ef37fa/.tests
+wt_s1_genes <- readr::read_tsv("data-raw/WT_S1.RSEM.genes.results")[1:10, ] # system.file('data-raw', 'WT_S1.RSEM.genes.results'))
+usethis::use_data(wt_s1_genes, overwrite = TRUE)
