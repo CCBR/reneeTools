011f0078c53da32dfc16bdc8fd2361c85f489625
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-12-28 21:39:40 -0500

    refactor: run_deseq function on reneeDS object

--- R/deseq2.R
+++ R/deseq2.R
@@ -1,34 +1,8 @@
-#' Create DESeq2 object from gene counts and sample metadata
-#'
-#' @param counts_tbl expected gene counts from RSEM as a data frame or tibble.
-#' @param meta_dat   sample metadata as a data frame with rownames as sample IDs.
-#' @param design     model formula for experimental design. Columns must exist in `meta_dat`.
-#'
-#' @return DESeqDataSet
-#' @export
-#'
-#' @examples
-#' sample_meta <- data.frame(
-#'   row.names = c("KO_S3", "KO_S4", "WT_S1", "WT_S2"),
-#'   condition = factor(c("knockout", "knockout", "wildtype", "wildtype"),
-#'     levels = c("wildtype", "knockout")
-#'   )
-#' )
-#' dds <- create_deseq_obj(gene_counts, sample_meta, ~condition)
-create_deseq_obj <- function(counts_tbl, meta_dat, design) {
-  # convert counts tibble to matrix
-  counts_mat <- counts_dat_to_matrix(counts_tbl)
-
-  # sample IDs must be in the same order
-  assertthat::are_equal(colnames(counts_mat), rownames(meta_dat))
-
-  return(DESeq2::DESeqDataSetFromMatrix(counts_mat, meta_dat, design))
-}
-
 #' Run DESeq2 on a reneeDataSet
 #'
 #' @param renee_ds reneeDataSet object
 #' @param design   model formula for experimental design. Columns must exist in `meta_dat`.
+#' @param ...      remaining variables are forwarded to `DESeq2::DESeq()`.
 #'
 #' @return reneeDataSet object with DESeq2 slot filled
 #' @export
@@ -44,12 +18,12 @@ create_deseq_obj <- function(counts_tbl, meta_dat, design) {
 #'   )
 #' )
 #' run_deseq2(rds, ~condition)
-run_deseq2 <- function(renee_ds, design) {
+run_deseq2 <- function(renee_ds, design, ...) {
   dds <- DESeq2::DESeqDataSetFromMatrix(
-    renee_ds$counts,
-    renee_ds$sample_meta,
+    renee_ds@counts,
+    renee_ds@sample_meta,
     design
   )
-  renee_ds$deseq2 <- DESeq2::DESeq(dds)
+  renee_ds@analyses$deseq2_ds <- DESeq2::DESeq(dds, ...)
   return(renee_ds)
 }
--- tests/testthat/test-deseq2.R
+++ tests/testthat/test-deseq2.R
@@ -1,26 +1,40 @@
 set.seed(20231228)
-test_that("create_deseq_obj works", {
-  sample_meta <-
-    data.frame(
-      row.names = c("KO_S3", "KO_S4", "WT_S1", "WT_S2"),
-      condition = factor(
-        c("knockout", "knockout", "wildtype", "wildtype"),
-        levels = c("wildtype", "knockout")
-      )
+test_that("run_deseq2 works", {
+  rds <- reneeDataSetFromFiles(
+    system.file(
+      "extdata",
+      "RSEM.genes.expected_count.all_samples.txt",
+      package = "reneeTools"
+    ),
+    system.file("extdata", "sample_metadata.tsv",
+      package = "reneeTools"
     )
-  dds <- create_deseq_obj(gene_counts, sample_meta, ~condition)
-
+  )
+  rds@sample_meta <- rds@sample_meta %>%
+    dplyr::mutate(condition = factor(condition,
+      levels = c("wildtype", "knockout")
+    ))
+  rds <- run_deseq2(rds, design = ~condition, fitType = "local")
+  dds <- rds@analyses$deseq2_ds
   expect_equal(
     dds@colData %>% as.data.frame(),
     structure(
-      list(condition = structure(
-        c(2L, 2L, 1L, 1L),
-        levels = c(
-          "wildtype",
-          "knockout"
+      list(
+        condition = structure(
+          c(2L, 2L, 1L, 1L),
+          levels = c(
+            "wildtype",
+            "knockout"
+          ),
+          class = "factor"
         ),
-        class = "factor"
-      )),
+        sizeFactor = c(
+          0.759835685651593,
+          0.718608223926169,
+          1.24466595457696,
+          1.68179283050743
+        )
+      ),
       class = "data.frame",
       row.names = c(
         "KO_S3",
@@ -33,9 +47,31 @@ test_that("create_deseq_obj works", {
     structure(
       list(
         counts.KO_S3 = c(25L, 16L, 19L),
-        counts.KO_S4 = c(22L, 10L, 26L),
+        counts.KO_S4 = c(
+          22L,
+          10L, 26L
+        ),
         counts.WT_S1 = c(74L, 0L, 10L),
-        counts.WT_S2 = c(104L, 0L, 8L)
+        counts.WT_S2 = c(
+          104L,
+          0L, 8L
+        ),
+        mu.1 = c(24.1516682192168, 13.3092987017581, 23.1890203839711),
+        mu.2 = c(22.8412375617529, 12.5871575689046, 21.9308214491443),
+        mu.3 = c(75.6181929866826, 0.158242295472865, 7.74702804399805),
+        mu.4 = c(102.175314069834, 0.213817014139956, 10.4677854923446),
+        H.1 = c(0.511844332569779, 0.504138999697003, 0.50668916671393),
+        H.2 = c(0.48815561528062, 0.495860729372594, 0.493310737076545),
+        H.3 = c(0.454896663393873, 0.499997728127273, 0.447711231007698),
+        H.4 = c(0.545103297274594, 0.499997728127273, 0.552288438770429),
+        cooks.1 = c(
+          0.00787282712563543,
+          0.0262422886768066,
+          0.0478403297046226
+        ),
+        cooks.2 = c(0.00740399240405431, 0.025741204681852, 0.0464156166543046),
+        cooks.3 = c(0.00250160501520332, 0.127533456822227, 0.0779681548830785),
+        cooks.4 = c(0.00307311736197857, 0.161328557073165, 0.100411826759898)
       ),
       class = "data.frame",
       row.names = c(
