3b96ed754eed7500576f6af224d8baaa05b6c27f
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-08-20 19:20:17 -0400

    feat: calc cpm

--- tests/testthat/test-counts.R
+++ tests/testthat/test-counts.R
@@ -18,7 +18,7 @@ test_that("counts_dat_to_matrix works", {
   )
 })
 
-test_that("calc_cpm works", {
+test_that("calc_cpm works on RENEE data", {
   sample_meta <- data.frame(
     sample_id = c("KO_S3", "KO_S4", "WT_S1", "WT_S2"),
     condition = factor(
@@ -26,12 +26,39 @@ test_that("calc_cpm works", {
       levels = c("wildtype", "knockout")
     )
   )
-  renee_ds <- create_reneeDataSet_from_dataframes(sample_meta, gene_counts) %>%
-    calc_cpm()
+  renee_ds <- create_reneeDataSet_from_dataframes(
+    sample_meta,
+    gene_counts %>% select(-GeneName)
+  )
+  renee_ds <- renee_ds %>% calc_cpm()
   cpm_edger <- gene_counts %>%
+    select(-GeneName) %>%
     counts_dat_to_matrix() %>%
     edgeR::cpm() %>%
     as.data.frame() %>%
     tibble::rownames_to_column("gene_id")
   expect_equal(renee_ds@counts$cpm, cpm_edger)
+})
+
+test_that("calc_cpm_df works on NIDAP data", {
+  df <- nidap_clean_raw_counts %>% as.data.frame()
+  trans.df <- df
+  trans.df[, -1] <- edgeR::cpm(as.matrix(df[, -1]))
+
+  expect_equal(
+    calc_cpm_df(df, gene_colname = "Gene"),
+    trans.df
+  )
+})
+test_that("calc_cpm_df preserves rownames", {
+  df <- nidap_clean_raw_counts %>%
+    as.data.frame() %>%
+    tail()
+  trans.df <- df
+  trans.df[, -1] <- edgeR::cpm(as.matrix(df[, -1]))
+
+  expect_equal(
+    calc_cpm_df(df, gene_colname = "Gene"),
+    trans.df
+  )
 })
